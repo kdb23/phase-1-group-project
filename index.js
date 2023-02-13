fbedfea8f812c9cb317d0b675ff4f6f192783b50
Beau Kim <beaukim3@gmail.com>
2023-02-13 16:30:04 -0500

    added initial JS functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 fetch('http://localhost:3000/exercises')
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(exercises => renderGains(exercises[0]) )
 
 // hi everyone!!!!!
 /* 
@@ -18,3 +18,34 @@ Project Requirements
             -click event (pulling up a card of the chosen difficulty of exercise)
             -mouseover (showing gif of the specific exercise)
 */
+
+const muscle = document.getElementById("muscle")
+const chest = document.getElementById("chest")
+const glutes = document.getElementById("glutes")
+const gains = document.getElementById("gains")
+const beginnerHeader = document.getElementById("beginner")
+const intermediateHeader= document.getElementById("intermediate")
+const expertHeader = document.getElementById("expert")
+
+const exerciseName = document.getElementById("name")
+const equipment = document.getElementById("equipment")
+const instructions = document.getElementById("instructions")
+
+
+function renderGains(exercises) {
+gains.addEventListener("click", e => {
+const imgBeginner = document.createElement("img")
+const imgIntermediate = document.createElement("img")
+const imgExpert = document.createElement("img")
+imgBeginner.src = exercises.image
+beginnerHeader.append(imgBeginner)
+imgIntermediate.src = exercises.image
+intermediateHeader.append(imgIntermediate)
+imgExpert.src = exercises.image
+expertHeader.append(imgExpert)
+
+exerciseName.innerText = exercises.name
+equipment.innerText = exercises.equipment
+instructions.innerText = exercises.instructions
+})
+}
