a04fe84d13e34da03d23d48697960e48aabba1b6
Raymond An <raymond_an@yahoo.com>
2023-02-13 11:21:39 -0500

    added project guidelines pt1

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ siteRes.addEventListener('click', e => {
 })
 
 // hi everyone!!!!!
+/* 
+Project Requirements
+    -HTML/CSS/JS    DONE
+    
+    -API or db.json
+        -atleast 5 Objects with 3 attributes
+        -must be asynchronous and use JSON format
+    
+    -entire app must include 3 distinct event listeners
+        -each event listener should have its own unique callback func
+        -click, submit, mouseover
+            -submit (search function to filter with specific dropdown list of excercises)
+            -click event (pulling up a card of the chosen difficulty of exercise)
+            -mouseover (showing gif of the specific exercise)
+*/
