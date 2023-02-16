38629594a1b278d04d4fc372fbf3997561b80f71
Beau Kim <beaukim3@gmail.com>
2023-02-16 10:44:54 -0500

    pseudo labels

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const beginner = document.getElementById('beginner')
+const intermediate = document.getElementById('intermediate')
+const expert = document.getElementById('expert')
+const gainsForm = document.getElementById("gains_form")
+const beginnerInfo = document.getElementById("beginnerInfo")
+const intermediateInfo = document.getElementById("intermediateInfo")
+const expertInfo = document.getElementById("expertInfo")
+const numLikes = document.getElementById('numLikes')
+const likesBtn = document.getElementById('likes')
+let value = parseInt(numLikes.innerText)
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+//Button
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+likesBtn.addEventListener('click', (e) => {
+    value++
+    numLikes.innerText = value
+})
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+//Muscle Selector
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 const changeEvent = muscle.addEventListener("change", e => {
     const chestClass = document.getElementsByClassName("chest")
     const glutesClass = document.querySelectorAll(".glutes")
@@ -29,168 +55,102 @@ fetch('http://localhost:3000/exercises')
        exercises.forEach(renderExercises);
        changeEvent;
     })
+    
 
-const beginner = document.getElementById('beginner')
-const intermediate = document.getElementById('intermediate')
-const expert = document.getElementById('expert')
-const gainsForm = document.getElementById("gains_form")
-const beginnerInfo = document.getElementById("beginnerInfo")
-const intermediateInfo = document.getElementById("intermediateInfo")
-const expertInfo = document.getElementById("expertInfo")
-
-
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+//Render Exercise Image/Info
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 function renderExercises(exerciseArray) {
     const image = document.createElement('img')
     const nameLi = document.createElement('li')
     const infoLi = document.createElement('li')
     image.src = exerciseArray.image
     image.alt = exerciseArray.name
+    
     if (exerciseArray.difficulty == 'beginner' && exerciseArray.muscle == "chest") {
-        image.className = "chest"
-        image.style.display = "none"
-        nameLi.className = "chestMouse"
-        nameLi.style.display = "none"
-        infoLi.className = "chestMouse"
-        infoLi.style.display = "none"
-        nameLi.innerText = exerciseArray.name
-        infoLi.innerText = exerciseArray.instructions
+        muscleStyleClass(exerciseArray.muscle)
         beginnerInfo.append(nameLi, infoLi)
         beginner.append(image)
     } else if (exerciseArray.difficulty == 'beginner' && exerciseArray.muscle == "glutes") {
-        image.className = "glutes"
-        image.style.display = "none"
-        nameLi.className = "glutesMouse"
-        nameLi.style.display = "none"
-        infoLi.className = "glutesMouse"
-        infoLi.style.display = "none"
-        nameLi.innerText = exerciseArray.name
-        infoLi.innerText = exerciseArray.instructions
+        muscleStyleClass(exerciseArray.muscle)
         beginnerInfo.append(nameLi, infoLi)
         beginner.append(image)
     } else if (exerciseArray.difficulty == 'intermediate' && exerciseArray.muscle == "chest") {
-        image.className = "chest"
-        image.style.display = "none"
-        nameLi.className = "chestMouse"
-        nameLi.style.display = "none"
-        infoLi.className = "chestMouse"
-        infoLi.style.display = "none"
-        nameLi.innerText = exerciseArray.name
-        infoLi.innerText = exerciseArray.instructions
+        muscleStyleClass(exerciseArray.muscle)
         intermediateInfo.append(nameLi, infoLi)
         intermediate.append(image)
     } else if (exerciseArray.difficulty == 'intermediate' && exerciseArray.muscle == "glutes") {
-        image.className = "glutes"
-        image.style.display = "none"
-        nameLi.className = "glutesMouse"
-        nameLi.style.display = "none"
-        infoLi.className = "glutesMouse"
-        infoLi.style.display = "none"
-        nameLi.innerText = exerciseArray.name
-        infoLi.innerText = exerciseArray.instructions
+        muscleStyleClass(exerciseArray.muscle)
         intermediateInfo.append(nameLi, infoLi)
         intermediate.append(image)
     } else if ( exerciseArray.difficulty == 'expert' && exerciseArray.muscle == "chest") {
-        image.className = "chest"
-        image.style.display = "none"
-        nameLi.className = "chestMouse"
-        nameLi.style.display = "none"
-        infoLi.className = "chestMouse"
-        infoLi.style.display = "none"
-        nameLi.innerText = exerciseArray.name
-        infoLi.innerText = exerciseArray.instructions
+        muscleStyleClass(exerciseArray.muscle)
         expertInfo.append(nameLi, infoLi)
         expert.append(image)
     } else if ( exerciseArray.difficulty == 'expert' && exerciseArray.muscle == "glutes") {
-        image.className = "glutes"
-        image.style.display = "none"
-        nameLi.className = "glutesMouse"
-        nameLi.style.display = "none"
-        infoLi.className = "glutesMouse"
-        infoLi.style.display = "none"
-        nameLi.innerText = exerciseArray.name
-        infoLi.innerText = exerciseArray.instructions
+        muscleStyleClass(exerciseArray.muscle)
         expertInfo.append(nameLi, infoLi)
         expert.append(image)
     }
     image.addEventListener("mouseover", (e) => {
         if (e.target.src === "https://media2.giphy.com/media/5t9IcXiBCyw60XPpGu/giphy.gif?cid=ecf05e478kgm8ozhfrjdecouhm3nik2ew1f5c1rnr3ddmlhc&rid=giphy.gif&ct=g") {
-            nameLi.style.display = "block";
-            infoLi.style.display = "block";
+            blockDisplay();
         } 
         if (e.target.src === "https://i0.wp.com/www.strengthlog.com/wp-content/uploads/2020/02/Smalb%C3%A4nk.gif?resize=600%2C600&ssl=1") {
-            nameLi.style.display = "block";
-            infoLi.style.display = "block";
+            blockDisplay();
         } 
         if (e.target.src === "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-3gVua4VSOcUTYmUbKO-2aY4esfaDnyPT9A&usqp=CAU") {
-            nameLi.style.display = "block";
-            infoLi.style.display = "block";
+            blockDisplay();
         } 
         if (e.target.src === "https://media.self.com/photos/57aa008dd077f2273cf20476/master/w_1600%2Cc_limit/STEP_UP_KNEE_LIFT.gif") {
-            nameLi.style.display = "block";
-            infoLi.style.display = "block";
+            blockDisplay();
         } 
         if (e.target.src === "https://assets.myworkouts.io/exercises-media/mGKGRBCHDAscPj8Bk/kneeling_squat_female_v16_gif_capoff.gif") {
-            nameLi.style.display = "block";
-            infoLi.style.display = "block";
+            blockDisplay();
         } 
         if (e.target.src === "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/barbell-exercises-hip-thrust-1549030719.gif?resize=480:*") {
-            nameLi.style.display = "block";
-            infoLi.style.display = "block";
+            blockDisplay();
         } 
     })
     image.addEventListener("mouseleave", (e) => {
         if (e.target.src === "https://media2.giphy.com/media/5t9IcXiBCyw60XPpGu/giphy.gif?cid=ecf05e478kgm8ozhfrjdecouhm3nik2ew1f5c1rnr3ddmlhc&rid=giphy.gif&ct=g") {
-            nameLi.style.display = "none";
-            infoLi.style.display = "none";
+            noneDisplay();
         } 
         if (e.target.src === "https://i0.wp.com/www.strengthlog.com/wp-content/uploads/2020/02/Smalb%C3%A4nk.gif?resize=600%2C600&ssl=1") {
-            nameLi.style.display = "none";
-            infoLi.style.display = "none";
+            noneDisplay();
         } 
         if (e.target.src === "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-3gVua4VSOcUTYmUbKO-2aY4esfaDnyPT9A&usqp=CAU") {
-            nameLi.style.display = "none";
-            infoLi.style.display = "none";
+            noneDisplay();
         } 
         if (e.target.src === "https://media.self.com/photos/57aa008dd077f2273cf20476/master/w_1600%2Cc_limit/STEP_UP_KNEE_LIFT.gif") {
-            nameLi.style.display = "none";
-            infoLi.style.display = "none";
+            noneDisplay();
         } 
         if (e.target.src === "https://assets.myworkouts.io/exercises-media/mGKGRBCHDAscPj8Bk/kneeling_squat_female_v16_gif_capoff.gif") {
-            nameLi.style.display = "none";
-            infoLi.style.display = "none";
+            noneDisplay();
         } 
         if (e.target.src === "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/barbell-exercises-hip-thrust-1549030719.gif?resize=480:*") {
-            nameLi.style.display = "none";
-            infoLi.style.display = "none";
+            noneDisplay();
         }
     })
-}
-
-const numLikes = document.getElementById('numLikes')
-const likesBtn = document.getElementById('likes')
-let value = parseInt(numLikes.innerText)
-
-likesBtn.addEventListener('click', (e) => {
-    value++
-    numLikes.innerText = value
-})
-
-// hi everyone!!!!!
-/* 
-Project Requirements
-    -HTML/CSS/JS    DONE
     
-    -API or db.json
-        -atleast 5 Objects with 3 attributes
-        -must be asynchronous and use JSON format
-        
-        -entire app must include 3 distinct event listeners
-        -each event listener should have its own unique callback func
-        -click, change, mouseover
-        -change (search function to filter with specific dropdown list of excercises)
-        -click event (pulling up a card of the chosen difficulty of exercise)
-        -mouseover (showing gif of the specific exercise)
-        */
-  
-       
-
+    function muscleStyleClass(muscle) {
+        image.className = `${muscle}`
+        image.style.display = "none"
+        nameLi.className = `${muscle}Mouse`
+        nameLi.style.display = "none"
+        infoLi.className = `${muscle}Mouse`
+        infoLi.style.display = "none"
+        nameLi.innerText = exerciseArray.name
+        infoLi.innerText = exerciseArray.instructions
+    }
+    function blockDisplay() {
+        nameLi.style.display = "block";
+        infoLi.style.display = "block";
+        }
+    function noneDisplay() {
+        nameLi.style.display = "none";
+        infoLi.style.display = "none";
+    }
+}
